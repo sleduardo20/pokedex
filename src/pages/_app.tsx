68e1b3590e6f98c7ccb949ae460555c08d6b7d89
sleduardo20 <sl.eduardo20@gmail.com>
2021-01-14 21:38:39 -0300

    (refactor)🧹: edit Head app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,14 +9,11 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Pokedex</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="A simple project starter to work TypeScript, React,NextJS and Styled Components"
-        />
+        <meta name="description" content="Project Pokedex" />
       </Head>
       <Component {...pageProps} />
       <GlobalStyles />
