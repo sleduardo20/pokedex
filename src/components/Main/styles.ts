438cd455f82baeac24cb9628a1ea8cdbcce717be
sleduardo20 <sl.eduardo20@gmail.com>
2021-01-14 21:02:31 -0300

    (chore)🚀: commit initial

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -13,22 +13,3 @@ export const Container = styled.main`
   justify-content: center;
   text-align: center;
 `;
-
-export const Logo = styled.img`
-  width: 25rem;
-  margin-bottom: 2rem;
-`;
-
-export const Title = styled.h1`
-  font-size: 2.5rem;
-`;
-
-export const Description = styled.h2`
-  font-size: 2rem;
-  font-weight: 400;
-`;
-
-export const Illutration = styled.img`
-  margin-top: 3rem;
-  width: min(30rem, 100%);
-`;
