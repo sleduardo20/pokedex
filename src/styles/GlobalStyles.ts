0d572520571974a83cf5cc706f26b0c3a6853644
sleduardo20 <sl.eduardo20@gmail.com>
2021-01-14 21:35:52 -0300

    (feat)✌: add config theme

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -5,6 +5,7 @@ export default createGlobalStyle`
     margin:0;
     padding:0;
     box-sizing:border-box;
+    text-decoration: none;
   }
 
   html{
@@ -16,7 +17,9 @@ export default createGlobalStyle`
   }
 
   body{
-    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', 
-    Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: ${({ theme }) => theme.font.fontFamily};
+    background-color: ${({ theme }) => theme.colors.white};
   }
+
+  
 `;
---
+++ src/styles/theme.ts
@@ -0,0 +1,35 @@
+export default {
+  spacings: {
+    small: '4rem',
+    medium: '8rem',
+    large: '12rem',
+    xlarge: '16rem',
+    xxlarge: '24rem',
+    xxxlarge: '32rem',
+  },
+  font: {
+    fontFamily:
+      "Roboto, --apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    bold: 800,
+    regular: 400,
+    light: 300,
+    medium: 700,
+
+    size: {
+      small: '1.2rem',
+      medium: '1.4rem',
+      large: '1.6rem',
+      xlarge: '2.4rem',
+      xxlarge: '4.8 rem',
+    },
+  },
+  colors: {
+    primary: '#CC0000',
+    secondary: '#FFCB05',
+    ternary: '#3D7DCA',
+    darkBlue: '#003A70',
+    white: '#FFFFFF',
+    gray: '#DFDFDF',
+    black: '#4F4F4F',
+  },
+} as const;
