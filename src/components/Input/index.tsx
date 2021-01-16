f698be1c4bc6f230a6433f4445d59c1212d36981
sleduardo20 <sl.eduardo20@gmail.com>
2021-01-16 16:16:39 -0300

    (feat)🧠: implementation Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,9 +1,74 @@
+import {
+  InputHTMLAttributes,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+
+import { useField } from '@unform/core';
+
 import * as S from './styles';
 
-const Input: React.FC = () => {
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, placeholder, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const [passwordShow, setPasswordShow] = useState(false);
+
+  const { fieldName, registerField } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [registerField, fieldName]);
+
+  const handelOnFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleBlur = useCallback(() => {
+    setIsFocused(false);
+  }, []);
+
+  const handleFilled = useCallback(text => {
+    if (text) {
+      setIsFilled(true);
+    } else {
+      setIsFilled(false);
+    }
+  }, []);
+
+  const handleShowPassword = useCallback(() => {
+    setPasswordShow(state => !state);
+  }, []);
+
   return (
-    <S.Container>
-      <input type="text" />
+    <S.Container isFocused={isFocused} isFilled={isFilled}>
+      <label htmlFor={name}>{placeholder}</label>
+      <input
+        ref={inputRef}
+        name={name}
+        onFocus={handelOnFocus}
+        onBlur={handleBlur}
+        type={passwordShow ? 'text' : 'password'}
+        onChange={e => handleFilled(e.target.value)}
+        {...rest}
+      />
+
+      {name === 'password' && isFilled && (
+        <button onClick={handleShowPassword} type="button">
+          {passwordShow ? 'Ocultar' : 'Mostrar'}
+        </button>
+      )}
     </S.Container>
   );
 };
