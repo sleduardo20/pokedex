e9e35e36d621290ddd2f1f2505b94435fde77e90
sleduardo20 <sl.eduardo20@gmail.com>
2021-01-17 09:49:54 -0300

    (docs)📑: config stories Input component

--- src/stories/02-Input.stories.tsx
+++ src/stories/02-Input.stories.tsx
@@ -1,10 +1,24 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
+import { Form } from '@unform/web';
 
-import Input from '../components/Input';
+import Input, { InputProps } from '../components/Input';
 
 export default {
   title: 'components/Input',
   component: Input,
+  args: {
+    name: 'email',
+  },
 } as Meta;
 
-export const Basic: Story = args => <Input {...args} />;
+export const Basic: Story<InputProps> = args => (
+  <div style={{ width: 450 }}>
+    <Form
+      onSubmit={() => {
+        console.log('ok');
+      }}
+    >
+      <Input placeholder="Email" {...args} />
+    </Form>
+  </div>
+);
