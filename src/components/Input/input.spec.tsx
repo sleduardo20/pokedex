a54faea24d46825ff4633d7fbb941ba68afc1060
sleduardo20 <sl.eduardo20@gmail.com>
2021-01-28 11:59:27 -0300

    (test)✔: create test styles Input component

--- src/components/Input/input.spec.tsx
+++ src/components/Input/input.spec.tsx
@@ -1,5 +1,5 @@
 import { screen } from '@testing-library/react';
-
+import userEvent from '@testing-library/user-event';
 import { renderWithTheme } from 'util/renderWithTheme';
 import { Form } from '@unform/web';
 
@@ -7,16 +7,25 @@ import Input from '.';
 
 describe('Input', () => {
   it('should be able render Input correctly', () => {
-    renderWithTheme(
+    const { container } = renderWithTheme(
       <Form
         onSubmit={() => {
-          console.log('ok');
+          jest.fn();
         }}
       >
-        <Input name="email" type="text" />
+        <Input name="email" type="text" placeholder="Usuario ou Email" />
       </Form>,
     );
 
-    expect(screen.getByRole('textbox')).toBeInTheDocument();
+    const input = screen.getByRole('textbox');
+
+    userEvent.click(input);
+
+    expect(input).toHaveFocus();
+    expect(input.parentElement).toHaveStyleRule('width', '34.8rem', {
+      modifier: ':focus-within',
+    });
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
