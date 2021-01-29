098a4f08faa0a7cc43662de281e12a9b810d6e29
sleduardo20 <sl.eduardo20@gmail.com>
2021-01-29 11:09:51 -0300

    (test)✔: create tests  background for color Button component

--- src/components/Button/button.spec.tsx
+++ src/components/Button/button.spec.tsx
@@ -15,4 +15,34 @@ describe('<Button/>', () => {
     expect(screen.getByText('Buscar')).toBeInTheDocument();
     expect(container.firstChild).toMatchSnapshot();
   });
+
+  it('should be able render background color primary', () => {
+    renderWithTheme(
+      <Button bg="primary">
+        <span>Entrar</span>
+      </Button>,
+    );
+
+    expect(screen.getByRole('button')).toHaveStyle({ background: '#CC0000' });
+  });
+
+  it('should be able render background color secondary', () => {
+    renderWithTheme(
+      <Button bg="secondary">
+        <span>Entrar</span>
+      </Button>,
+    );
+
+    expect(screen.getByRole('button')).toHaveStyle({ background: '#FFCB05' });
+  });
+
+  it('should be able render background color darkBlue', () => {
+    renderWithTheme(
+      <Button bg="darkBlue">
+        <span>Entrar</span>
+      </Button>,
+    );
+
+    expect(screen.getByRole('button')).toHaveStyle({ background: '#003A70' });
+  });
 });
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,7 +3,7 @@ import { ButtonHTMLAttributes } from 'react';
 import { Container } from './styles';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  bg: 'primary' | 'secondary' | 'darkBlue';
+  bg?: 'primary' | 'secondary' | 'darkBlue';
 }
 
 const Button: React.FC<ButtonProps> = ({
