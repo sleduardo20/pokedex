5f66788903791bdbbcc5530cb59753d76213c50d
sleduardo20 <sl.eduardo20@gmail.com>
2021-02-04 22:15:27 -0300

    (docs)📑: add mockCard in stories Card component

--- src/stories/04-Card.stories.tsx
+++ src/stories/04-Card.stories.tsx
@@ -1,10 +1,16 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import Card from '../components/Card';
+import Card, { CardProps } from '../components/Card';
+import mockCard from '../components/Card/mock';
 
 export default {
   title: 'components/Card',
   component: Card,
+  args: {
+    src: mockCard.imageUrl,
+    title: mockCard.name,
+    size: 'normal',
+  },
 } as Meta;
 
-export const Basic: Story = args => <Card {...args} />;
+export const Basic: Story<CardProps> = args => <Card {...args} />;
