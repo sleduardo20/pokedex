301a9985e5ad6b2d67c13c33e19937181526575b
sleduardo20 <sl.eduardo20@gmail.com>
2021-03-07 17:23:43 -0300

    (styles)🎨: add styles in link to  Card component

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,6 +1,6 @@
-import styled, { css } from 'styled-components';
+import styled, { css, DefaultTheme } from 'styled-components';
 
-import { CardProps, ImageProps } from '.';
+import { CardProps } from '.';
 
 type ContainerProps = Pick<CardProps, 'size'>;
 
@@ -17,10 +17,20 @@ const containerModifiers = {
 
 export const Container = styled.div<ContainerProps>`
   ${({ size }) => css`
-    border-radius: 0.8rem;
+    border-radius: 1.8rem;
+    box-shadow: 1px 1px 7px rgba(0, 0, 0, 0.4);
+    transition: 0.1s ease-in-out;
+    z-index: 10;
 
-    > img {
-      width: 100%;
+    & :hover {
+      transform: scale(1.03);
+    }
+
+    a {
+      > img {
+        width: 100%;
+        height: 100%;
+      }
     }
 
     ${!!size && containerModifiers[size]}
