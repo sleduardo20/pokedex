c9da116ae96638d169a507155c58bbe47487bdff
sleduardo20 <sl.eduardo20@gmail.com>
2021-03-08 10:34:05 -0300

    (feat)🧠: create Loader component

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles';
+
+const Loader = () => {
+  return (
+    <S.Container>
+      <S.Pokeball src="/img/pokeball.svg" />
+      <S.Base />
+    </S.Container>
+  );
+};
+
+export default Loader;
