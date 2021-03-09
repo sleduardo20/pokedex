fc76060e462bcecfe5b51424cace9a1a189760ba
sleduardo20 <sl.eduardo20@gmail.com>
2021-03-08 21:53:10 -0300

    (feat)✌: initial animation in Content component

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,10 +1,34 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
-export const Content = styled.div`
+const Wrapper = styled(motion.div)`
   width: min(110rem, 100%);
-  height: 100vh;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
+
+const easing = [0.6, -0.05, 0.01, 0.99];
+
+const fadeInUp = {
+  initial: {
+    y: -40,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: { duration: 1, ease: easing },
+  },
+};
+
+const Content: React.FC = ({ children }) => {
+  return (
+    <Wrapper initial={fadeInUp.initial} animate={fadeInUp.animate}>
+      {children}
+    </Wrapper>
+  );
+};
+
+export default Content;
