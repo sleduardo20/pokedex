210240bcdf8ffa74f86cf74ac5d5b2c161929b18
sleduardo20 <sl.eduardo20@gmail.com>
2021-03-10 19:39:40 -0300

    (feat)🧠: create connection with api

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,7 +1,18 @@
-import TemplateSearch from '../templates/Search';
+import { GetServerSideProps } from 'next';
+import api from 'services/api';
+import TemplateSearch, { SearchProps } from '../templates/Search';
 
-const Search = () => {
-  return <TemplateSearch />;
+const Search = (props: SearchProps) => {
+  return <TemplateSearch {...props} />;
 };
-
 export default Search;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const listPokemons = await api.get('cards').then(response => response.data);
+
+  return {
+    props: {
+      listPokemons,
+    },
+  };
+};
