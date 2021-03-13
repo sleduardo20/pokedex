61e2f01b2b357bb5f376a7acf3f97d5f96ecf740
sleduardo20 <sl.eduardo20@gmail.com>
2021-03-13 10:41:18 -0300

    (feat)⚙: edit types in component Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,28 +2,58 @@ import Link from 'next/link';
 
 import * as S from './styles';
 
-export interface CardProps {
+type Image = {
+  small: string;
+  large: string;
+};
+
+type Attacks = {
   name: string;
-  size: 'normal' | 'large';
-  images: {
-    small: string;
-    large: string;
-  };
-  slug: string;
+  damage: string;
+  text: string;
+};
+
+type Weaknesses = {
+  type: string;
+  value: string;
+};
+
+type Resistances = {
+  type: string;
+  value: string;
+};
+
+type Set = {
+  id: string;
+  name: string;
+};
+
+export interface CardProps {
+  id: string;
+  name: string;
+  supertype?: string;
+  level?: string;
+  hp?: string;
+  types?: [];
+  evolvesTo?: [];
+  images: Image;
+  rules?: [];
+  attacks?: Attacks[];
+  weaknesses?: Weaknesses[];
+  resistances?: Resistances[];
+  retreatCost?: [];
+  set?: Set;
+  artist?: string;
+  rarity?: string;
 }
 
-const Card: React.FC<CardProps> = ({
-  name,
-  images,
-  size = 'normal',
-  children,
-  slug,
-}) => {
+const Card: React.FC<CardProps> = ({ name, images, children }) => {
   return (
-    <S.Container size={size}>
-      <Link href={`http://localhost:3000/details/${slug}`} passHref>
+    <S.Container size="normal">
+      <Link href={`http://localhost:3000/details/${name}`}>
         <a>
           <img src={images.large} alt={name} />
+
           {children}
         </a>
       </Link>
