f135c5dcbc5810d1c4b7266cc60b273456ccc594
sleduardo20 <sl.eduardo20@gmail.com>
2021-03-27 05:50:16 -0300

    (chore)🔧: alter name variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_HOST,
-  headers: { 'X-Api-Key': process.env.NEXT_PUBLIC_KEY },
+  headers: { 'X-Api-Key': process.env.POKEMONIO_KEY },
 });
 
 export default api;
