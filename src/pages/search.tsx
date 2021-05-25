c6dcdf0285b932345682b6df35473e4d41a8e9ad
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-25 14:32:40 -0300

    ♻ refactor (Page Search): remove getStaticProps

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,30 +1,6 @@
-import { Image } from 'components/Card';
-import { GetStaticProps } from 'next';
-import api from 'services/api';
 import TemplateSearch, { SearchProps } from '../templates/Search';
 
 const Search = (props: SearchProps) => {
   return <TemplateSearch {...props} />;
 };
 export default Search;
-
-export const getStaticProps: GetStaticProps = async () => {
-  const data = await api.get('cards').then(response => {
-    const dataFormated = response.data.data.map(
-      (item: { id: string; name: string; images: Image }) => ({
-        id: item.id,
-        name: item.name,
-        images: item.images,
-      }),
-    );
-
-    return dataFormated;
-  });
-
-  return {
-    props: {
-      data,
-    },
-    revalidate: 60 * 60 * 24, // 24hs
-  };
-};
