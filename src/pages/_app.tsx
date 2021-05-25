fd3436cbd6f4c10a3cbc73909357c76e86d660eb
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-25 14:33:38 -0300

    ⚡ feat (App): add ToastProvider and NextProgressiveBar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { ToastProvider } from 'react-toast-notifications';
+import NextProgressBar from 'nextjs-progressbar';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyles from 'styles/GlobalStyles';
@@ -8,14 +10,26 @@ import theme from '../styles/theme';
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Head>
-        <title>Pokedex</title>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="shortcut icon" href="/img/favicon.png" type="image/x-icon" />
-        <meta name="description" content="Project Pokedex" />
-      </Head>
-      <Component {...pageProps} />
-      <GlobalStyles />
+      <ToastProvider>
+        <Head>
+          <title>Pokedex</title>
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            rel="shortcut icon"
+            href="/img/favicon.png"
+            type="image/x-icon"
+          />
+          <meta name="description" content="Project Pokedex" />
+        </Head>
+        <Component {...pageProps} />
+        <NextProgressBar
+          color="#003A70"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+        />
+        <GlobalStyles />
+      </ToastProvider>
     </ThemeProvider>
   );
 }
