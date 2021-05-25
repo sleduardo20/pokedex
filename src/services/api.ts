0063ce3b4b5d3d06f0c497d4c954fa0a9f1f4d8c
sleduardo20 <sl.eduardo20@gmail.com>
2021-05-25 14:37:39 -0300

    ➕ chore (dependencies): add react-toast-notifications and nextjs-progressbar

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_HOST,
   headers: { 'X-Api-Key': process.env.POKEMONIO_KEY },
 });
-
-export default api;
